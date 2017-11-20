8c2de1c64d8e89748ed2f894152f6f14249868c0
frederic-cote23 <frederic.cote23@gmail.com>
2017-11-20 15:16:52 -0500

    First Time
    Mindsweeper 2

---
+++ Front-End Intensive/minesweeper /src/minesweeper.js
@@ -0,0 +1,12 @@
+let board = [[' ', ' ',  ' '],[' ', ' ',  ' '],[' ', ' ',  ' ']];
+const printBoard = (board) => {
+    console.log('Current Board');
+    console.log(board[0].join('|'));
+    console.log(board[1].join('|'));
+    console.log(board[2].join('|'));
+};
+
+printBoard(board);
+board[0][1] = 1;
+board[1][0] = 'B';
+printBoard(board);
